10f7decb9151dee9b433b57f9bf94271ea217883
ktechb <k_techevo@yahoo.co.jp>
2021-05-06 07:02:04 +0900

    add hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust web server tutorial
+https://doc.rust-jp.rs/book-ja/ch20-01-single-threaded.html
+
+# log
+5/6 start
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        handle_connection(stream)
+
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    
+
 }
