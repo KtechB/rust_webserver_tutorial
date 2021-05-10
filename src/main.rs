650c62263b0201dd1ab9db367570fff2ff1239a8
ktechb <k_techevo@yahoo.co.jp>
2021-05-11 06:55:02 +0900

    finish single thread sction

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,13 +17,27 @@ fn main() {
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    let mut file = File::open("hello.html").unwrap();
+    let get  = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\n\r\n\r", "hello.html")
+    }else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\n\r\n\r", "hello.html")
+    }
+
+    else{
+        ("HTTP/1.1 404 NOT FOUNT\n\r\n\r", "404.html")
+    };
+    let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+
+    let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
     
 
 }
